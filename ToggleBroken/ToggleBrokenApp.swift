ba3981fd6898c7e46493e5c4cad7008cbdc79531
Alexander Cyon <alex.cyon@gmail.com>
2022-03-15 08:43:54 +0100

    Initial Commit

---
+++ ToggleBroken/ToggleBrokenApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToggleBrokenApp.swift
+//  ToggleBroken
+//
+//  Created by Alexander Cyon on 2022-03-15.
+//
+
+import SwiftUI
+
+@main
+struct ToggleBrokenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
